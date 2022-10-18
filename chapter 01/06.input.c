7ba567c3a5525d77374d5e4cd99a0c958c1401cd
nayeemmas <ebrahim.khalil.nayeem@gmail.com>
2022-10-19 02:18:00 +0600

    addition with taling INPUT.

---
+++ chapter 01/06.input.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int a, b;
+	printf("Enter the value of a: ");
+	scanf("%d", &a);
+	printf("Enter the value of b: ");
+	scanf("%d", &b);
+
+	printf("The value of a & b: %d, %d.\n", a, b);
+
+	return (0);
+}
