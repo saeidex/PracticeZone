f6504ea31c11037f22f810428454f0e6fb2244a7
nayeemmas <ebrahim.khalil.nayeem@gmail.com>
2022-10-28 15:10:29 +0600

    learned about & added power function

--- chapter 02/01_arithmetic_instructions.c
+++ chapter 02/01_arithmetic_instructions.c
@@ -17,16 +17,25 @@ int main(void)
   printf("The value of z*(a/b) is: %d.\n", z * (a / b));
 
   int c = z * b;
-  printf("The value of c is: %d", c);
+  printf("The value of c is: %d.\n\n", c);
 
   // reminder
   printf("5 when devided by 2 leaves a reminder of: %d.\n", 5 % 2);
-  printf("25 when devided by 3 leaves a reminder of: %d\n", 25 % 3);
+  printf("25 when devided by 3 leaves a reminder of: %d.\n\n", 25 % 3);
 
   int x, y;
-  printf("Enter the value of x & y: ");
-  scanf("%d %d", &x, &y);
-  printf("%d when devided by %d leaves a reminder of: %d.\n", x, y, x % y);
+  printf("Enter the value of- \n");
+  printf("x: ");
+  scanf("%d", &x);
+  printf("y: ");
+  scanf("%d", &y);
+  printf("%d when devided by %d leaves a reminder of: %d.\n\n", x, y, (x % y));
+
+  /* there is  no operator to perform exponentiation
+  ! Incorrect-->
+  printf("The value of 5 ^ 4 is: %d.\n", 5 ^ 4); // will not produce 5 to the power 4!
+  */
+  printf("The value of 5 to the power 4 is: %.0f.\n", pow(5, 4));
 
   return 0;
 }
